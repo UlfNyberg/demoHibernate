b2193c752e6a752360a602dd3bd04fa4432d02ab
UlfNyberg <72010004+UlfNyberg@users.noreply.github.com>
2021-04-15 15:05:00 +0200

    added hard-coded Kompisar via Bean in Main

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import com.example.demo.models.Kompis;
+import com.example.demo.repositories.KompisRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +14,18 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
+
+    @Bean
+    public CommandLineRunner demo (KompisRepository repository){
+        return (args) -> {
+            //hard-coded a few friends
+            repository.save(new Kompis(1,"Mr. Burns", "efef@efef.cmo", "0702-232"));
+            repository.save(new Kompis(2,"Ralph", "efef@efef.cmo", "0702-2333232"));
+            repository.save(new Kompis(3,"Wiggums", "efef@efef.cmo", "073-45645"));
+
+            for (Kompis kompis : repository.findAll()){
+                System.out.println(kompis.toString());
+            }
+        };
+    }
 }
